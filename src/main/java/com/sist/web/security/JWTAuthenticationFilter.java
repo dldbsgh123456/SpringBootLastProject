ea44072a0edb88da64c51f711225fdf2846b247d
dldbsgh124584-commits <dldbsgh124584@gmail.com>
2026-09-01 11:26:45 +0900

    9/1

---
+++ src/main/java/com/sist/web/security/JWTAuthenticationFilter.java
@@ -0,0 +1,5 @@
+package com.sist.web.security;
+// 인증 
+public class JWTAuthenticationFilter {
+
+}
---
+++ src/main/java/com/sist/web/security/JWTAuthenticationProvider.java
@@ -0,0 +1,5 @@
+package com.sist.web.security;
+// 키 생성
+public class JWTAuthenticationProvider {
+
+}
